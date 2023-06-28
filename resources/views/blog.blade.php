e402ae6b5dc3c38c6fbae7e78f53fec015415bc7
gisehweb <gisehbc@gmail.com>
2023-06-28 18:54:50 -0300

    Controllador creado PostController mediante el comando php artisan make:controller, modificacion de web.php agregando el metodo get y el PostController::class

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,5 +5,7 @@ meta-description="Blog meta description"
 >
 
 <h1>Blog</h1>
-
+  @foreach ($posts as $post)
+    <h2>{{$post['title']}}</h2>
+  @endforeach
 </x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 /* metodo get recibe dos parametros la url el nombre de nuestro dominio, 
 se retorna la vista welcome*/ 
 //rutas de este proyecto
@@ -24,5 +16,6 @@ Route::get('/', function () {
 });
 Route::view('/', '/welcome')->name ('home');
 Route::view('/contact', 'contact')->name ('contact');
-Route::view('/blog', 'blog')->name('blog');
+Route::get('/blog', [PostController::class, 'index'])-> name('blog');
+
 Route::view('/about', 'about')->name('about');
